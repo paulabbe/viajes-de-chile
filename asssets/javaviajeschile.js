ba443da3db051b51d5e3850ca97a4af3548be971
paulina <labbepaulina@gmail.com>
2022-08-06 20:35:34 -0400

    cambiosjavascrip

--- asssets/javaviajeschile.js
+++ asssets/javaviajeschile.js
@@ -2,12 +2,19 @@ $(document).ready(function(){
 
 
       
-    $(".btn").click(function(){
+    $(".boton").dblclick(function(){
     $(this).on(alert("el correo fue enviado correctamente..." ))    
     })
 
-   
 
+  $('[data-bs-toggle="tooltip"]').tooltip();
+
+
+$(document).scroll(function (){
+    const y = $("html").scrolltop();
+
+    y > 300 ? $("nav").addclass("nav-black") : $("nav").removeClass("nav-black")
+})
 
 
 
